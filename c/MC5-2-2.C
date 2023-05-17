fe69afc8834cfcc9d7a499b3d55c8a16e23bf3b4
iTusharAmbaliya <tusharambaliya34@gmail.com>
2023-05-17 18:35:06 +0530

    17-05-23|

---
+++ c/MC5-2-2.C
@@ -0,0 +1,63 @@
+
+/*
+#include<stdio.h>
+#include<conio.h>
+#define pi 3.14
+void main()
+{
+	int r=25;
+       //	const float pi=3.14;
+	clrscr();
+
+	printf("area of circle : %.2f",pi*r*r);
+
+	getch();
+
+}
+*/
+/*
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int l=12;
+	float b=15.5;
+	clrscr();
+
+	printf("area of rectangle : %.4f",l*b);
+
+	getch();
+}
+  */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int l=6,h=3;
+
+	clrscr();
+
+	printf("area of tringal:%d",(l*h)/2);
+
+	getch();
+
+
+}
+
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+	int
+
+	clrscr();
+
+	printf(area of );
+
+	getch();
+}
+
+
+
+
+
